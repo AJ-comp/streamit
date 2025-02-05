72770da46ff1755a3f94997af9a696cc93fe2251
AJ-comp <69152847+AJ-comp@users.noreply.github.com>
2025-02-05 19:06:43 +0900

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import os
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import Ollama
+from langchain.prompts import PromptTemplate
+from langchain.chains.llm import LLMChain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chains import RetrievalQA
 
+st.title("Ask Anything: Auto-Select PDFs with RAG")
